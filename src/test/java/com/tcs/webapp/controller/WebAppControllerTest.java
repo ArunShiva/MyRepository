992654147b1272f10de6c432f6ffa79edaf2a90d
ArunShiva <gmrarunn@gmail.com>
2020-12-31 16:52:11 +0530

    Added Unit Test Cases

--- src/test/java/com/tcs/webapp/controller/WebAppControllerTest.java
+++ src/test/java/com/tcs/webapp/controller/WebAppControllerTest.java
@@ -1,13 +1,12 @@
 package com.tcs.webapp.controller;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 import org.hamcrest.Matchers;
@@ -16,7 +15,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -26,20 +24,27 @@ import com.tcs.webapp.model.UserDetails;
 import com.tcs.webapp.service.impl.RetriveUserDetails;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+/*
+ * @WebAppConfiguration
+ * 
+ * @ContextConfiguration
+ * 
+ * @ComponentScan(basePackages = {"com"})
+ */
 public class WebAppControllerTest {
 
 	private MockMvc mockMvc;
 
-    @Mock
-    private RetriveUserDetails retriveUserDetails;
+	@Mock
+	private RetriveUserDetails retriveUserDetails;
 
-    @InjectMocks
-    private WebAppController controller;
-    
-    List<UserDetails> userList= new ArrayList<UserDetails>();
+	@InjectMocks
+	private WebAppController controller;
 
-    @Before
-	public void setUp() throws Exception {
+	List<UserDetails> userList = new ArrayList<UserDetails>();
+
+	@Before
+	public void testsetUp() throws Exception {
 		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
 
 		UserDetails user1 = new UserDetails("1", "Arun", "Chennai", "arun@gmail.com");
@@ -51,23 +56,24 @@ public class WebAppControllerTest {
 		userList.add(user3);
 	}
 
-    @Test
-    public void testSimpleJson() throws Exception {
-        mockMvc.perform(get("/services/json")
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-    }
-    
-    @Test
-    public void testGetUserList() throws Exception {
-    	
-    	when(retriveUserDetails.retriveUserDetails()).thenReturn(userList);
-    	
-		 mockMvc.perform(get("/services/getUserList").contentType(MediaType.APPLICATION_JSON)
-	                .accept("application/json"))
-	                .andExpect(status().is(200))
-	                .andExpect(jsonPath("$.status", Matchers.is("Success")));
-		
+	@Test
+	public void testSimpleJson() throws Exception {
+		mockMvc.perform(get("/services/json").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+	}
+
+	@Test
+	public void testGetUserList() throws Exception {
+
+		when(retriveUserDetails.retriveUserDetails()).thenReturn(userList);
+
+		mockMvc.perform(get("/services/getUserList").contentType(MediaType.APPLICATION_JSON).accept("application/json"))
+				.andExpect(status().is(200)).andExpect(jsonPath("$.status", Matchers.is("Success")));
+
+	}
+
+	@Test
+	public void testSomething() {
+		assertTrue(true);
 	}
 
 }
